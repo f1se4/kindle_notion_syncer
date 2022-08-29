a593d46245720a9f89ec34967726f3a582c81b49
f1se4 <sergi.garcia.munoz@gmail.com>
2022-08-29 12:11:02 +0200

    change title by extractor

--- query_notion_database.py
+++ query_notion_database.py
@@ -170,7 +170,7 @@ def create_page(title, author, paragraph_list):
             "database_id": database_ID
         },
         "properties": {
-            "Title": {
+            "Extracto": {
                 "title": [
                     {
                         "type": "text",
